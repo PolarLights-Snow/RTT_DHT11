a203eb7772d23bbb0092a4ad9ceab1b64e088555
unknown <1440179893@qq.com>
2021-12-10 10:54:14 +0800

    D

---
+++ applications/main.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020-2021, Bluetrum Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2020/12/10     greedyhao    The first version
+ */
+
+/**
+ * Notice!
+ * All functions or data that are called during an interrupt need to be in RAM.
+ * You can do it the way exception_isr() does.
+ */
+
+#include <rtthread.h>
+#include "board.h"
+
+
+int main(void)
+{
+
+
+    while (1)
+    {
+
+    }
+
+
+}
+
